0ac041f48c0f79e2f521ce211726a8404f866535
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2021-12-07 14:44:13 -0800

    upadte

--- task4.py
+++ task4.py
@@ -11,7 +11,18 @@ j = input("enter an ADVERB")
 k = input("enter an FOOD")
 l = input("enter an THINGS")
 
-print("Today we picked apple from",str(a),"Orchard.""I had no idea there were so many different varieties of apples.""I ate",str(b),"apples straight off the tree that tasted like",str(d),".""Then there was a",str(e),"apple that looked like a",str(g),".""When our bag was full, we went on a free hay ride to",print(h),"and back.""It ended at a hay pile where we got to",str(i),str(j),".""I can hardly wait to get home and cook with the apples." "We are going to make apple",str(k),"and",str(l),"pies!")
+print("Today we picked apple from John's Orchard. I had no idea there were so many different varieties of apples. I ate purple apples straight off the tree that tasted like chicken. Then there was a happy apple that looked like a nun.  When our bag was full, we went on a free hay ride to Ohio and back. It ended at a hay pile where we got to clumsy crazy. I can hardly wait to get home and cook with the apples. We are going to make apple cookie and worm pies!%0AActual:%0Aenter an PERSON'senter an ADJECTIVEenter an FOODenter an ADJECTIVEenter an NOUNenter an PLACEenter an VERBenter an ADVERBenter an FOODenter an THINGSOhio%0AToday we picked apple from John%0D Orchard.I had no idea there were so many different varieties of apples.I ate purple%0D apples straight off the tree that tasted like chicken%0D .Then there was a happy%0D apple that looked like a nun%0D .When our bag was full, we went on a free hay ride to None and back.It ended at a hay pile where we got to clumsy%0D crazy%0D .I can hardly wait to get home and cook with the apples.We are going to make apple cookie%0D and worm%0D pies!")
+
+"""
+print("Today we picked apple from",str(a),"Orchard."
+"I had no idea there were so many different varieties of apples."
+"I ate",str(b),"apples straight off the tree that tasted like",str(d),"."
+"Then there was a",str(e),"apple that looked like a",str(g),"."
+"When our bag was full, we went on a free hay ride to",print(h),"and back."
+"It ended at a hay pile where we got to",str(i),str(j),"."
+"I can hardly wait to get home and cook with the apples." 
+"We are going to make apple",str(k),"and",str(l),"pies!")
+"""
 
 """
 Assignment 5:
@@ -24,4 +35,3 @@ Then there was a _ADJECTIVE_ apple that looked like a _NOUN_.
 When our bag was full, we went on a free hay ride to _PLACE_ and back. It ended at a hay pile where we got to _VERB_ _ADVERB_.
 I can hardly wait to get home and cook with the apples. We are going to make apple _FOOD_ and _THINGS_ pies!
 """
-
